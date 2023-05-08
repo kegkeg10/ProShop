c605e190a708d172be784e2db5cfbcc32a25a90a
Nick Garner <nickgcs13@gmail.com>
2023-05-07 20:49:49 -0500

    backend

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from "./config/db.js";
-import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import productRoutes from './routes/productRoutes.js';
 const port = process.env.PORT || 5000;
 
 connectDB(); //connect to mongo db from db.js
@@ -15,5 +16,7 @@ app.get('/', (req,res) => {
 
 app.use('/api/products', productRoutes)
 
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port,() => console.log(`Server is running on port ${port}`))
