1b63dca1dc1e535c3259011819b5e8dd64b0e87c
Nick Garner <nickgcs13@gmail.com>
2023-05-14 16:48:58 -0500

    cantredirect

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -83,5 +83,3 @@ return (
 }
 
 export default LoginScreen
-
-
--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -10,13 +10,19 @@ export const usersApiSlice = apiSlice.injectEndpoints({
                 body: data,
             }),
         }),
+        register: builder.mutation({
+            query: (data) => ({
+                url: `${USERS_URL}`,
+                method: 'POST',
+                body: data,
+            }),
+        }),
         logout: builder.mutation({
             query: () => ({
                 url: `${USERS_URL}/logout`,
                 method: 'POST',
-            })
-        })
-    }),
-});
-
-export const { useLoginMutation, useLogoutMutation } = usersApiSlice
+                }),
+            }),
+        }),
+    });
+export const { useLoginMutation, useLogoutMutation, useRegisterMutation } = usersApiSlice
